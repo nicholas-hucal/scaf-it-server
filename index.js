d47776efb1f7a4e0f01c3ccb3074917a67019f98
Nicholas Hucal <nhucal@mac.com>
2022-03-25 21:09:56 -0600

    further refactor to mvc the github auth process

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ const expressSession = require('express-session');
 const helmet = require('helmet');
 const cors = require('cors');
 const passport = require('passport');
-const GitHubStrategy = require('passport-github2').Strategy;
-const knex = require('knex')(require('./knexfile.js').development);
+const authModel = require('./models/authModel.js');
 const app = express();
-
 
 //MIDDLEWARE
 app.use(
@@ -28,54 +26,9 @@ app.use(express.json());
 app.use(helmet());
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(
-    new GitHubStrategy({
-        clientID: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: process.env.GITHUB_CALLBACK_URL
-    },
-        (_accessToken, _refreshToken, profile, done) => {
-            console.log('GitHub profile:', profile);
-            knex('user')
-                .select('id')
-                .where({ github_id: profile.id })
-                .then(user => {
-                    if (user.length) {
-                        done(null, user[0]);
-                    } else {
-                        knex('user')
-                            .insert({
-                                github_id: profile.id,
-                                avatar_url: profile._json.avatar_url,
-                                username: profile.username
-                            })
-                            .then(userId => {
-                                done(null, { id: userId[0] });
-                            })
-                            .catch(err => {
-                                console.log('Error creating a user', err);
-                            });
-                    }
-                })
-                .catch(err => {
-                    console.log('Error fetching a user', err);
-                });
-        }
-    )
-);
-passport.serializeUser((user, done) => {
-    done(null, user.id);
-});
-passport.deserializeUser((userId, done) => {
-    knex('user')
-        .where({ id: userId })
-        .then(user => {
-            done(null, user[0]);
-        })
-        .catch(err => {
-            console.log('Error finding user', err);
-        });
-});
+passport.use(authModel.checkAddUser);
+passport.serializeUser(authModel.serializeUser);
+passport.deserializeUser(authModel.deserializeUser);
 
 // ROUTES
 const homeRoute = require('./routes/homeRoute.js');
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,11 +3,8 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 
 router.get('/github', authController.connect);
-
 router.get('/github/callback', authController.auth, authController.callback);
-
 router.get('/profile', authController.getProfile);
-
 router.get('/logout', authController.logout);
 
 module.exports = router;
